f7b3aba652db7d87d318d0d3f94b28f18f5a4207
Richárd Pónusz <ponuszrichard93@gmail.com>
2019-11-03 10:36:02 +0100

    section 8 completed, weather app is completely done

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,30 @@
-console.log('Client side js file is loaded');
+console.log("Client side js file is loaded");
+
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.getElementById('message-1');
+const messageTwo = document.getElementById('message-2');
+
+messageOne.textContent = '';
+messageTwo.textContent = '';
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const location = search.value;
+
+  messageOne.textContent = 'Loading...';
+
+  fetch(`http://localhost:3000/weather?address=${location}`).then((res) => {
+    res.json().then(data => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+        messageTwo.textContent = '';
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      }
+    })
+  })
+});
